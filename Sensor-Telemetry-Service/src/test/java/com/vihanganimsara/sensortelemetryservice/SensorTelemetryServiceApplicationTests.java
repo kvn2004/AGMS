4bb433fec9481147611cf47001597447c1850be8
kvn2004 <knimsaravihanga@gmail.com>
2026-04-09 04:56:54 +0530

    feat(Sensor-Telemetry-Service): initialize project structure with Maven and Spring Boot setup

---
+++ Sensor-Telemetry-Service/src/test/java/com/vihanganimsara/sensortelemetryservice/SensorTelemetryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vihanganimsara.sensortelemetryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorTelemetryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
